7d9c1ef25b06f93856efe013df64a7bbd6760954
pablordrrbl <prodrr05@estudiantes.unileon.es>
2016-06-23 09:53:13 +0200

    Added whitelisted .gitignore file

---
+++ lisp/my-packages.el
@@ -0,0 +1,36 @@
+;;prrobles-packages.el --- Package installation
+
+;; package.el
+(require 'package)
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; A list of packages to ensure are installed at launch
+(defvar my-packages
+  '(better-defaults
+    ein
+    elpy
+    flycheck
+    material-theme
+    py-autopep8)
+    "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
+(provide 'prrobles-packages)
+
+;; prrobles-packages.el ends here
